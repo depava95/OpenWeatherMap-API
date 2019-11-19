b96d4eca3899588073040e0dc6d4745050219f44
Ihor Biedin <biedin.developer@gmail.com>
2019-11-19 22:01:46 +0200

    Initial commit

---
+++ src/test/java/com/biedin/openweather/OpenweatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biedin.openweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenweatherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
