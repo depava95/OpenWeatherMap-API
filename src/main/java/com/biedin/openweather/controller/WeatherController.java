fb51e78f65e26905b608cda60d6c6883751cca51
Ihor Biedin <biedin.developer@gmail.com>
2019-11-20 20:43:53 +0200

    Add models

---
+++ src/main/java/com/biedin/openweather/controller/WeatherController.java
@@ -0,0 +1,8 @@
+package com.biedin.openweather.controller;
+
+/**
+ * @author Ihor Biedin 20.11.2019
+ */
+
+public class WeatherController {
+}
---
+++ src/main/java/com/biedin/openweather/exception/DataNotFoundException.java
@@ -0,0 +1,8 @@
+package com.biedin.openweather.exception;
+
+/**
+ * @author Ihor Biedin 20.11.2019
+ */
+
+public class DataNotFoundException {
+}
