e8c221b474d817306b1c54c799a654ce37b058ea
Ihor Biedin <biedin.developer@gmail.com>
2019-11-21 02:38:33 +0200

    Refactor

---
+++ src/main/java/com/biedin/openweather/configuration/MvcConfig.java
@@ -0,0 +1,8 @@
+package com.biedin.openweather.configuration;
+
+/**
+ * @author Ihor Biedin 20.11.2019
+ */
+
+public class MvcConfig {
+}
---
+++ src/main/java/com/biedin/openweather/exception/InvalidCityNameException.java
@@ -0,0 +1,8 @@
+package com.biedin.openweather.exception;
+
+/**
+ * @author Ihor Biedin 21.11.2019
+ */
+
+public class InvalidCityNameException {
+}
